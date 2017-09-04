55f21da86592f989114b81ba4940fa8d7b52cbe6
Abhilash Upadhya <abhilashupadhya@Abhilashs-MacBook-Pro.local>
2017-09-03 20:30:15 -0400

    Print the cycle when detected

--- IsCycleInDirectedGraph.java
+++ IsCycleInDirectedGraph.java
@@ -14,36 +14,55 @@ class IsCycleInDirectedGraph{
 		for(int i=0; i<v ; i++){
 			adj[i] = new LinkedList<Integer>();
 		}
-
-
 	}
 
 	void addEdge(int u, int v){
 		adj[u].add(v);
 	}
 
-	boolean DFSVisit(int u, boolean[] visited, Stack<Integer> stack){
-		visited[u] = true;
+	boolean DFSVisit(int u, Integer[] parent, Stack<Integer> stack){
+		//visited[u] = true;
+		//System.out.println("u: " +u);
 		stack.push(u);
-		System.out.print(u + " --> ");
 		Iterator<Integer> i = adj[u].listIterator();
 		while(i.hasNext()){
 			int n = i.next();
-			if     (!visited[n]) 			return DFSVisit(n, visited, stack);
-			else if(stack.search(n) != -1) 	return true;
+			//System.out.println("   n: " + n);
+			if     (parent[n] == -999) {
+					parent[n] = u;
+					return DFSVisit(n, parent, stack);
+			}		
+			else if(stack.search(n) != -1) 	{
+				int x = u;
+				System.out.println("Cycle Found!!");
+				
+				while(x >= 0){
+
+					System.out.print(x + " --> ");
+					x= parent[x];
+				}
+				return true;
+			}
 		}
+		stack.pop();
 		return false;
 
-	}
-
+		
+	}	
 
 	boolean isCycle(){
-		boolean visited[] = new boolean[V];
+		Integer parent[] = new Integer[V];
+		for(int i= 1; i<V; i++)
+			parent[i] = -999;
+		
+		parent[0] = -1;
+
 		Stack<Integer> stack = new Stack<Integer>();
+		if(DFSVisit(0, parent, stack)) return true;
 
 		for(int i=0; i< V; i++){
-			if(!visited[i])
-				if(DFSVisit(i, visited, stack)) return true;
+			if(parent[i] == -999)
+				if(DFSVisit(i, parent, stack)) return true;
 		}
 		return false;
 	}
@@ -58,9 +77,9 @@ class IsCycleInDirectedGraph{
 		g.addEdge(2,4);
 		g.addEdge(3,0);
 
-		if(g.isCycle())
-			System.out.println(" Contains a cycle");
+		if(g.isCycle()) System.out.println(" Contains a cycle");
 		else System.out.println(" No cycle");
+
 
 
 	}
